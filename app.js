7c6a16488b4a664450f126239b68808b201486b8
rrisskyy <riskykurniawan945@gmail.com>
2021-10-05 06:10:40 +0800

    Starter

--- app.js
+++ app.js
@@ -7,16 +7,52 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true});
 
 const wikiSchema = mongoose.Schema({
-    title:string,
-    content:string
+    title:String,
+    content:String
 });
 
-app.use(express.static('public'));
+const Article = mongoose.model('Article', wikiSchema)
+
+
+
+app.get('/articles', (req, res) => {
+    Article.find({}, (err, articlesFound) => {
+        res.send(articlesFound);
+    })
+})
+
+app.post('/articles', (req, res) => {
+    const newArticle = new Article ({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save(err => {
+        if(err) {
+            res.send(err);
+        } else {
+            res.send("Sukses menambahkan item!");
+        }
+    });
+})
+
+app.delete('/articles', (req, res) => {
+    Article.deleteMany({}, (err) => {
+        if(err){
+            res.send("Berhasil Menghapus Item!")
+        } else {
+            res.send(err)
+        }
+    })
+})
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
